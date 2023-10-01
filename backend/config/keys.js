0e0f39cde09ab2de9c7e9b3aab55b0fc3161a420
Dennis Lee <dennislee1499@gmail.com>
2023-10-01 16:11:37 -0400

    passport jwt setup

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,10 +1,13 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-};
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+// };
 
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  isProduction: process.env.NODE_ENV === "production",
+    secretOrKey: process.env.SECRET_OR_KEY,
+    mongoURI: process.env.MONGO_URI,
+    isProduction: process.env.NODE_ENV === "production",
 };
+
+
 
 
